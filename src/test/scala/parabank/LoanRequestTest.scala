8e409691b32d82ac93d6b8e4a8d8db3adf9bd3a8
Juan Arboleda <juan.arboleda16@udea.edu.co>
2026-05-01 00:10:21 -0500

    Fix: Resolver conflictos de merge en LoanRequestTest y pom.xml

--- src/test/scala/parabank/LoanRequestTest.scala
+++ src/test/scala/parabank/LoanRequestTest.scala
@@ -12,33 +12,16 @@ class LoanRequestTest extends Simulation {
   // 1 Http Conf
   val httpConf = http.baseUrl(url)
     .acceptHeader("application/json")
-<<<<<<< HEAD
-=======
-    .contentTypeHeader("application/json")
-    .basicAuth(username, password)
-
-  // Request body with loan details
-  val loanRequestBody =
-    s"""{
-       |  "customerId": "$customerId",
-       |  "amount": "$loanAmount",
-       |  "downPayment": "$downPayment"
-       |}""".stripMargin
->>>>>>> 34215ba4ecc396ab52fa03d285f4dbe0ca66abcd
 
   // 2 Scenario Definition
   val scn = scenario("Loan Request - Carga Concurrente")
     .exec(
       http("Request Loan")
         .post("/requestLoan")
-<<<<<<< HEAD
         .queryParam("customerId", customerId)
         .queryParam("amount", loanAmount)
         .queryParam("downPayment", downPayment)
         .queryParam("fromAccountId", fromAccountId)
-=======
-        .body(StringBody(loanRequestBody))
->>>>>>> 34215ba4ecc396ab52fa03d285f4dbe0ca66abcd
         .check(status.is(200))
     )
 
